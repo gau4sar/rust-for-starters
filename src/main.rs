1509fbee73fc0ed06b838ffb6c69b965e7c059f3
Gaurav Sarma <gaurav4sarma@gmail.com>
2022-09-13 22:03:07 +0530

    Primitive/Scaler types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
+
+    // Primitive types
+
+    let unsignedInt: u8 = 10;
+    let signedInt: i8 = -10;
+    let float: f32 = 1.2;
+
+    println!(
+        "unsignedInt: {}, signedInt: {}, float: {}",
+        unsignedInt, signedInt, float
+    );
+
+    let letter = "ch12123";
+    let emoji = "\u{1F600}";
+
+    println!("letter: {}, emoji: {}", letter, emoji);
+
+    let isBoolean = true || false;
+    println!("isBoolean: {}", isBoolean);
 }
