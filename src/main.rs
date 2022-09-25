fae50e3701c956bd741333c8f81101569fa55d63
Gaurav Sarma <gaurav4sarma@gmail.com>
2022-09-25 15:47:51 +0530

    Mutability, Arrays and Slice

--- src/main.rs
+++ src/main.rs
@@ -23,14 +23,42 @@ fn main() {
     //Compound types
 
     // Arrays
-    let arr: [u8;3] = [1,2,3];
+    let arr: [u8; 3] = [1, 2, 3];
 
     //print structure of the array
     println!("arr: {:?}", arr);
 
-    //Tuples
+    //Tuples - a DS that can hold unlimited elements of different types
+    let tuple: (u8, bool, f32) = (5, true, 2.1);
+    let tuple2 = (3, 5);
+
+    print!("first {}, second {}, third {}", tuple.0, tuple.1, tuple.2);
+    println!("{:?}", tuple2);
+
+    let (a, b, c) = tuple;
+
+    //destructuring a tuple
+    println!("first{}, second{}, third{}", a, b, c);
+
+    // Function call
     println!("{}", is_even(4));
-    
+
+    //Mutability - needs to be explicitly defined
+    let mut num = 5;
+    num = 3;
+    println!("{}", num);
+
+    //Slice
+    let arr = [1, 2, 3, 4, 5, 6];
+    let slice = &arr[1..6]; // [1,2,3] required when don't know the length of the array
+    borrowing_slice(arr, slice)
+}
+
+fn borrowing_slice(arr: [u8; 6], slice: &[u8]) {
+    println!("Array : {:?}", arr);
+    println!("Slice : {:?}", slice);
+    println!("Lenght : {:?}", slice.len());
+    println!("{}, {}", slice[0], slice[1]);
 }
 
 pub fn is_even(num: u8) -> bool {
